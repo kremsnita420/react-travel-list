9d20ec7c9022b408da5f35e76e8bfbe8fe62e56f
Safet Duranovic <duranovic.safet@gmail.com>
2023-06-09 11:15:49 +0200

    Lifting state
    Lift the state up from the Form component to App component and pass it to the List component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,109 @@
-import './index.css';
+import { useState } from 'react';
+
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: true },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	// Handle adding items to list
+	function handleAddItems(item) {
+		setItems(items => [...items, item]);
+	}
+
 	return (
 		<div className='app'>
 			<Logo />
-			<Form />
-			<PackingList />
+			<Form onAddItems={handleAddItems} />
+			<PackingList items={items} />
 			<Stats />
 		</div>
 	);
 }
 
+//! Logo component
 function Logo() {
 	return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+//! Form component
+function Form({ onAddItems }) {
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState(1);
+
+	// Event handler
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		// Create new item
+		if (!description) return;
+		const newItem = { description, quantity, packed: false, id: Date.now() };
+		console.log(newItem);
+
+		onAddItems(newItem);
+
+		// Reset form
+		setDescription('');
+		setQuantity(1);
+	}
+
 	return (
-		<div className='add-form'>
+		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your 🥰 trip?</h3>
+			<select
+				value={quantity}
+				onChange={e => setQuantity(Number(e.target.value))}
+			>
+				{/* Create array of options and map them */}
+				{Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type='text'
+				placeholder='Item...'
+				value={description}
+				onChange={e => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
+	);
+}
+
+//! List component
+function PackingList({ items }) {
+	return (
+		<div className='list'>
+			<ul>
+				{items.map(item => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
 		</div>
 	);
 }
 
-function PackingList() {
-	return <div className='list'>LIST</div>;
+//! List item component
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
+//! Footer component
 function Stats() {
 	return (
-		<footer>You have X items on your list, and you already packed X</footer>
+		<footer className='stats'>
+			You have X items on your list, and you already packed X (X%)
+		</footer>
 	);
 }
